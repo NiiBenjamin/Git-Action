af1771e963146f131ed98c56b8525b468d447935
NiiBenjamin <adom_a@yahoo.co.uk>
2024-05-13 17:30:40 +0100

    third commit

--- main.tf
+++ main.tf
@@ -3,10 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 
   tags = {
     Name = "ExampleInstance"
   }
 }
+
